f930b1ed618d5941fba700ba2a842bb51a721d00
zhujiang2 <zhujiang2@lenovo.com>
2023-10-20 14:46:57 +0800

    做个假的

--- app/src/main/java/com/zj/smart/MainPage.kt
+++ app/src/main/java/com/zj/smart/MainPage.kt
@@ -5,9 +5,11 @@ package com.zj.smart
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
@@ -37,12 +39,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zj.smart.utils.RoomType
+import com.zj.smart.utils.SmartType
 import com.zj.smart.utils.StaggeredGridData
 import com.zj.smart.utils.roomType
 import com.zj.smart.utils.staggeredGridDataMutableList
@@ -104,7 +109,7 @@ fun MainPage(toScan: () -> Unit, toDetails: (StaggeredGridData) -> Unit) {
                 }
                 showPermission.value = false
             }
-            VrPager()
+            VrPager(toDetails)
 
             //填充数据
             LazyVerticalStaggeredGrid(modifier = Modifier
@@ -129,7 +134,7 @@ fun MainPage(toScan: () -> Unit, toDetails: (StaggeredGridData) -> Unit) {
 
 @Composable
 @OptIn(ExperimentalFoundationApi::class)
-private fun VrPager() {
+private fun VrPager(toDetails: (StaggeredGridData) -> Unit) {
     val scope = rememberCoroutineScope()
     val pagerState = rememberPagerState(
         initialPage = 0,
@@ -138,13 +143,36 @@ private fun VrPager() {
         pages.size
     }
 
+    val current = LocalContext.current
     HorizontalPager(
         modifier = Modifier,
         state = pagerState,
         key = { pages[pagerState.currentPage] },
         pageContent = { page ->
             Card(modifier = Modifier.padding(horizontal = 16.dp)) {
-                VrView(resIds[page])
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(210.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    VrView(resIds[page])
+                    Text(
+                        text = "1111111",
+                        color = Color.Transparent,
+                        modifier = Modifier.clickable {
+                            toDetails(
+                                StaggeredGridData(
+                                    R.string.display, R.drawable.display, arrayListOf(
+                                        SmartType.Ordinary,
+                                        SmartType.HighPlay,
+                                        SmartType.Placement,
+                                        SmartType.Working,
+                                    ), arrayListOf(RoomType.Bedroom, RoomType.LivingRoom)
+                                )
+                            )
+                        })
+                }
             }
         }
     )
